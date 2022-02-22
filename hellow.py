9812fece56a57e1fad80830af8178d0d8712af0a
Sathusan Rajanikanth <97081481+Rj-Sathusan@users.noreply.github.com>
2022-02-22 13:52:28 +0530

    done

--- hellow.py
+++ hellow.py
@@ -1,9 +1,1 @@
-from Tkinter import *
-
-root = Tk()
-var = StringVar()
-label = Label( root, textvariable=var, relief=RAISED )
-
-var.set("Hey!? How are you doing\njhu?Hey!? How are you doing\njhu?Hey!? How are you doing\njhu?Hey!? How are you doing\njhu?Hey!? How are you doing\njhu?Hey!? How are you doing\njhu?")
-label.pack()
-root.mainloop()
+ok=input("ready?")
