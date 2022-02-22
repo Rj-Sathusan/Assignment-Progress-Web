44df1d3045340cd94f905078cdd635f329fd6d72
Sathusan Rajanikanth <97081481+Rj-Sathusan@users.noreply.github.com>
2022-02-22 13:49:53 +0530

    tkinter

--- hellow.py
+++ hellow.py
@@ -1,1 +1,9 @@
-print("hellow")
+from tkinter import *
+
+root = Tk()
+var = StringVar()
+label = Label( root, textvariable=var, relief=RAISED )
+
+var.set("Hey!? How are you doing\njhu?Hey!? How are you doing\njhu?Hey!? How are you doing\njhu?Hey!? How are you doing\njhu?Hey!? How are you doing\njhu?Hey!? How are you doing\njhu?")
+label.pack()
+root.mainloop()
