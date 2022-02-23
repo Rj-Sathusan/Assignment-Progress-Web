9d20b2cf9fd55195fdd77b80e92fcbfcf7f83f55
Sathusan Rajanikanth <97081481+Rj-Sathusan@users.noreply.github.com>
2022-02-23 13:30:56 +0530

    ok

--- hellow.py
+++ hellow.py
@@ -5,14 +5,4 @@ from datetime import datetime
 st.title("Hello GeeksForGeeks !!!")
 
 
-mydb  = mysql.connector.connect(
-  host="sql6.freemysqlhosting.net", user="sql6474318",
-  password="Q3Bq46Z4Vd", database="sql6474318")
-  
-
-mycursor = mydb.cursor()
-
-mycursor.execute("SELECT * FROM Try")
-
-Result = mycursor.fetchall()
-st.write(Result)
+st.write("Result")
