9d2913e457079bb4e1298475de24dedc176f3d88
Sathusan Rajanikanth <97081481+Rj-Sathusan@users.noreply.github.com>
2022-02-24 09:12:46 +0530

    Update hellow.py

--- hellow.py
+++ hellow.py
@@ -6,7 +6,7 @@ import mysql.connector
 # create connection object
 mydb  = mysql.connector.connect(
   host="sql6.freemysqlhosting.net", user="sql6474318",
-  password="Q3Bq46Z4Vd", database="sql6474318")
+  password="Q3Bq46Z4Vd", database=st.secrets["db"])
   
 
 mycursor = mydb.cursor()
