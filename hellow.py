12aa41c1901af6a521a52f2aad16a8a6de6df996
Sathusan Rajanikanth <97081481+Rj-Sathusan@users.noreply.github.com>
2022-02-24 08:59:01 +0530

    Update hellow.py

--- hellow.py
+++ hellow.py
@@ -1,18 +1,17 @@
 import streamlit as st
 # import required modules
 import mysql.connector
-st.write("DB username:", st.secrets["db_username"])  
+ok=st.secrets["db_username"]
 # create connection object
-'''
+
+# create connection object
 mydb  = mysql.connector.connect(
-  host=(st.secrets[db_host]), user=(st.secrets[db_username]),
-  password=(st.secrets[db_password]), database=(st.secrets[db_username]))
- 
+  host="sql6.freemysqlhosting.net", user=ok,
+  password="Q3Bq46Z4Vd", database="sql6474318")
+  
 
 mycursor = mydb.cursor()
 
 mycursor.execute("SELECT * FROM Try")
 
 Result = mycursor.fetchall()
-st.write(Result)
-'''
