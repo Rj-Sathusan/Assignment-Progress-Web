a75ccc1a97e55dffba7b507a8100fc0be68ef78c
Sathusan Rajanikanth <97081481+Rj-Sathusan@users.noreply.github.com>
2022-02-24 08:25:43 +0530

    ok

--- hellow.py
+++ hellow.py
@@ -1,24 +1,4 @@
-# Python program to create a table
-
-from tkinter import *
-
-
-class Table:
-	
-	def __init__(self,root):
-		
-		# code for creating table
-		for i in range(total_rows):
-			for j in range(total_columns):
-				
-				self.e = Entry(root, width=15, fg='black',
-							font=('Arial',12))
-				
-				self.e.grid(row=i, column=j)
-				self.e.insert(END, lst[i][j])
-
-
-
+import streamlit as st
 # import required modules
 import mysql.connector
   
@@ -36,13 +16,4 @@ Result = mycursor.fetchall()
 
 head=[("ID","NAME")]
 lst = head+Result
-
-# find total number of rows and
-# columns in list
-total_rows = len(lst)
-total_columns = len(lst[2])
-
-# create root window
-root = Tk()
-t = Table(root)
-root.mainloop()
+st.write(list)
