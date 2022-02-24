fcf23ba88c5c43f94737700aaa8ebf85158be94f
Sathusan Rajanikanth <97081481+Rj-Sathusan@users.noreply.github.com>
2022-02-24 09:05:03 +0530

    Update hellow.py

--- hellow.py
+++ hellow.py
@@ -6,7 +6,7 @@ ok=st.secrets["db_username"]
 
 # create connection object
 mydb  = mysql.connector.connect(
-  host="sql6.freemysqlhosting.net", user=ok,
+  host="sql6.freemysqlhosting.net", user="sql6474318",
   password="Q3Bq46Z4Vd", database="sql6474318")
   
 
