d057194dc9080cab40bfe2097cab1c821ca6f9aa
Sathusan Rajanikanth <97081481+Rj-Sathusan@users.noreply.github.com>
2022-03-10 16:20:15 +0530

    Update hellow.py

--- hellow.py
+++ hellow.py
@@ -11,8 +11,16 @@ mydb  = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-mycursor.execute("SELECT * FROM assignment_details")
+sql = "SELECT * FROM assignment_details WHERE Student_code=1003"
+mycursor.execute(sql)
+#mycursor.execute("SELECT * FROM assignment_details")
 
 Result = mycursor.fetchall()
 
-st.write(Result)
+st.write('Student ID : ',Result[0][0],'\nName : ',Result[0][1],'\nStarting Date : ',Result[0][2],'\nCompleted Assignments : ',Result[0][3])
+
+if Result[0][3]>14:
+        st.write("Statue : Completed")
+elif Result[0][3]<14:
+        st.write("Statue : ",(15-Result[0][3]),"more pending...")
+
