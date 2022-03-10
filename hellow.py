6c334e5f687427f74b19983d138a1a21ce2099d0
Sathusan Rajanikanth <97081481+Rj-Sathusan@users.noreply.github.com>
2022-03-10 17:23:32 +0530

    Update hellow.py

--- hellow.py
+++ hellow.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 import mysql.connector
 st.markdown("<u><h1 style='text-align: center; color: red;'>Assignment Progress System</h1><u><br>", unsafe_allow_html=True)
-st.markdown("img src="idm-campus.jpg"")
+st.markdown("img src='idm-campus.jpg'")
 img = Image.open("idm-campus.jpg")
 st.image(img, width=500)
 
