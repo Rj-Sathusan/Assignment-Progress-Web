ac387dd4bc44da98de73973319007070093d63c7
Sathusan Rajanikanth <97081481+Rj-Sathusan@users.noreply.github.com>
2022-03-11 00:05:48 +0530

    Update hellow.py

--- hellow.py
+++ hellow.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 import mysql.connector
-from streamlit import caching
+
 
 st.markdown("<u><h1 style='text-align: center; color: red;'>Assignment Progress System</h1><u><h4 style='text-align: center; color: white;'>IDM EASTERN CAMPUES</h4><u><br>", unsafe_allow_html=True)
 st.markdown("<br><br>", unsafe_allow_html=True)
@@ -13,7 +13,6 @@ mydb  = mysql.connector.connect(
 
 name = st.text_input("Enter Your Student ID","") 
 if(st.button('Submit')):
-  caching.clear_cache()
   sql = "SELECT * FROM assignment_details WHERE Student_code="+name
   mycursor = mydb.cursor()
   mycursor.execute(sql)
