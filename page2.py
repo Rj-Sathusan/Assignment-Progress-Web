f642be08afdb3027edb7fd242ab1ab74bfedb7bb
Sathusan Rajanikanth <97081481+Rj-Sathusan@users.noreply.github.com>
2022-03-11 00:02:44 +0530

    Create page2.py

---
+++ page2.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from PIL import Image
+import mysql.connector
+from streamlit import caching
+
+st.markdown("<u><h1 style='text-align: center; color: red;'>Assignment2 Progress System</h1><u><h4 style='text-align: center; color: white;'>IDM EASTERN CAMPUES</h4><u><br>", unsafe_allow_html=True)
+st.markdown("<br><br>", unsafe_allow_html=True)
