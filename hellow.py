b749512a2bab5d4e6611c370e8b187442a19efda
Sathusan Rajanikanth <97081481+Rj-Sathusan@users.noreply.github.com>
2022-04-12 12:55:09 +0530

    Update hellow.py

--- hellow.py
+++ hellow.py
@@ -10,9 +10,9 @@ mydb  = mysql.connector.connect(
   host=st.secrets["db_host"], user=st.secrets["db_user"],
   password=st.secrets["db_pass"], database=st.secrets["db"])
   
-hobby = st.selectbox("",
+who = st.selectbox("",
                      ['I am a Student', 'I am a Teacher'])
-name = st.text_input("Enter Your option ID","") 
+name = st.text_input("Enter Your ",who," ID","") 
 if(st.button('Submit')):
   sql = "SELECT * FROM assignment_details WHERE Student_code="+name
   mycursor = mydb.cursor()
