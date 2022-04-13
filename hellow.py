b75dc1402074b8382e02e8fa2c6f408286e02b64
Sathusan Rajanikanth <97081481+Rj-Sathusan@users.noreply.github.com>
2022-04-13 12:54:03 +0530

    Update hellow.py

--- hellow.py
+++ hellow.py
@@ -15,6 +15,9 @@ agree = st.checkbox('I agree',value = True)
 hobbies = st.multiselect("Hobbies: ",
                          ['Dancing', 'Reading', 'Sports'])
 hobbies=['Dancing']
+
+hobbies
+
 with col1:
       who = st.selectbox("I am a",
                      ['Student', 'Teacher'])
